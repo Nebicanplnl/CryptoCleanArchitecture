ae0f7359a0fec3ff4126286fdbea37b6080f2473
Nebican Planalı <nebicanplanali@gmail.com>
2024-11-02 17:52:12 +0300

    sssssss

--- app/src/main/java/com/example/cryptocleanarchitecture/util/Constants.kt
+++ app/src/main/java/com/example/cryptocleanarchitecture/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.cryptocleanarchitecture.util
 
 object Constants{
  //https://api.collectapi.com/economy/cripto
-    const val API_KEY = "apikey 6qDy2cXLIHDDrPHG79RnOl:2aLFCo56Ega9Jvb3BbF94n"
+    const val API_KEY = "apikey-Token s52561641656515q512368854a8s2168948"
     const val BASE_URL = "https://api.collectapi.com/"
     const val CONTENT_TYPE = "application/json"
 }
